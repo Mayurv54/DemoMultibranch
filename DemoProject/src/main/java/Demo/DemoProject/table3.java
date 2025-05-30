02307b0c60fa8d6dfc6a96f45702cc23dcadf178
Mayur54 <mayurvelani54@gmail.com>
2025-05-30 18:23:43 +0530

    table3 added

---
+++ DemoProject/src/main/java/Demo/DemoProject/table3.java
@@ -0,0 +1,13 @@
+package Demo.DemoProject;
+
+
+public class table3 {
+
+	public static void main(String[] args) {
+		int a = 3;
+		for (int i=1;i<=10;i++) {
+			int b = a*i;
+			System.out.println(a+"*"+i+"="+b);
+		}
+	}
+}
